4462b3d33412fe3cae0b0c302b02ee85ca0d89e5
Daniel Berthereau <Daniel.github@Berthereau.net>
2020-03-02 00:00:00 +0100

    Added a stop to the undo job.

--- src/Job/Undo.php
+++ src/Job/Undo.php
@@ -1,21 +1,39 @@
 <?php
 namespace OaiPmhHarvester\Job;
 
+use Omeka\Api\Exception\NotFoundException;
 use Omeka\Job\AbstractJob;
+use Omeka\Stdlib\Message;
 
 class Undo extends AbstractJob
 {
     public function perform()
     {
-        $jobId = $this->getArg('jobId');
-        $api = $this->getServiceLocator()->get('Omeka\ApiManager');
         // TODO Improve memory management for deletion of previous harvest and allow to stop.
-        $response = $api->search('oaipmhharvester_entities', ['job_id' => $jobId]);
-        $harvestEntities = $response->getContent();
-        if ($harvestEntities) {
-            foreach ($harvestEntities as $harvestEntity) {
+
+        /** @var \Omeka\Api\Manager $api */
+        $services = $this->getServiceLocator();
+        $logger = $services->get('Omeka\Logger');
+        $api = $services->get('Omeka\ApiManager');
+
+        $jobId = $this->getArg('jobId');
+        $harvestEntities = $api->search('oaipmhharvester_entities', ['job_id' => $jobId])->getContent();
+        if (!$harvestEntities) {
+            return;
+        }
+
+        foreach ($harvestEntities as $key => $harvestEntity) {
+            if ($this->shouldStop()) {
+                $logger->warn(new Message(
+                    'The job "Undo" was stopped: %d/%d resources processed.', // @translate
+                    $key, count($harvestEntities)
+                ));
+                break;
+            }
+            try {
                 $api->delete('oaipmhharvester_entities', $harvestEntity->id());
                 $api->delete($harvestEntity->resourceType(), $harvestEntity->entityId());
+            } catch (NotFoundException $e) {
             }
         }
     }
